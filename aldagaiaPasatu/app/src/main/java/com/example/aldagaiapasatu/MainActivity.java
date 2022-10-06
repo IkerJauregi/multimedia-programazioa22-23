371c3ef6890085ad28c77c4f0e5a662b9ac88e43
Iker <ikerj2001@gmail.com>
2022-10-06 22:24:56 +0200

    zeozer

--- aldagaiaPasatu/app/src/main/java/com/example/aldagaiapasatu/MainActivity.java
+++ aldagaiaPasatu/app/src/main/java/com/example/aldagaiapasatu/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.aldagaiapasatu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText zen1, zen2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Zenbakiak aldagaietan sartu
+        zen1 = (EditText)findViewById(R.id.zen1);
+        zen2 = (EditText)findViewById(R.id.zen2);
+    }
+    public void gehitu(View v){
+        Intent intent = new Intent(this, JasoActivity.class);
+        intent.putExtra("KEY_SENDER", zen1.getText().toString());
+        startActivity(intent);
     }
 }
